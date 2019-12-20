37ff2c5dcf2657e420c9321fbadde5bbebccc9cb
MAC_DEV <mcamacho1990@icloud.com>
2019-12-20 02:56:59 -0500

    fixEditStrainEditDB

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 
+
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'strainAPI'
 app.config["MONGO_URI"] = "mongodb+srv://root:r00tUser@strainapicluster-euclv.mongodb.net/strainAPI?retryWrites=true&w=majority"
@@ -16,9 +17,56 @@ def get_strains():
 
 @app.route('/add_strain')
 def add_strain():    
-    return render_template('addstrain.html', strains=mongo.db.strains.find())
+    return render_template('addstrain.html', strains=mongo.db.strains.find())   
 
+@app.route('/insert_strain', methods=['POST'])
+def insert_strain():
+    strains = mongo.db.strains
+    strains.insert_one(request.form.to_dict())
+    return redirect(url_for('get_strains'))     
 
+@app.route('/edit_strain/<strain_id>')
+def edit_strain(strain_id):
+    the_strain = mongo.db.strains.find_one({"_id": ObjectId(strain_id)})
+    # all_types = mongo.db.strains.find()
+    all_types = mongo.db.type.find()
+    return render_template('editstrain.html', strain=the_strain, type=all_types)
+
+@app.route('/DB')
+def get_DB():
+    return render_template("DB-all.html", strains=mongo.db.strains.find())
+
+@app.route('/add_DB')
+def add_DB():    
+    return render_template('DB-add.html', strains=mongo.db.strains.find())    
+
+@app.route('/insert_DB', methods=['POST'])
+def insert_DB():
+    strains = mongo.db.strains
+    strains.insert_one(request.form.to_dict())
+    return redirect(url_for('get_DB'))
+
+@app.route('/edit_DB/<strain_id>')
+def edit_DB(strain_id):
+    db_strain = mongo.db.strains.find_one({"_id": ObjectId(strain_id)})
+    db_types = mongo.db.type.find()
+    type_list = [typeX for typeX in db_types]
+    return render_template('DB-edit.html', strain=db_strain , type=type_list)
+
+@app.route('/update_DB/<strain_id>', methods=["POST"])
+def update_DB(strain_id):
+    strains = mongo.db.strains
+    strains.update( {'_id': ObjectId(strain_id)},
+    {
+        'strain_name':request.form.get('name'),
+        'category_name':request.form.get('type'),
+        'task_description': request.form.get('description'),
+        'due_date': request.form.get('firstTried'),
+        'is_urgent':request.form.get('isDank')
+    })
+    return redirect(url_for('get_DB'))    
+
+      
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=os.environ.get('PORT'),
